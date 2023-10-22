2acc601f5d85a51770d42092d5e2b83414656757
theshivamverma <vermashivam527@gmail.com>
2023-10-23 05:23:29 +0530

    feat: drag and update added

--- src/components/Timeline/index.jsx
+++ src/components/Timeline/index.jsx
@@ -1,20 +1,79 @@
-import React, { useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import { useAudio } from "../../context";
-
-import styles from "./Timeline.module.css";
 import { calculateOffset, calculateWidth } from "../../common/utils";
 
+import styles from "./Timeline.module.css";
+
 const TimelineRow = ({ audioData, totalDuration }) => {
-  const { audioName, duration, startPoint, bgColor } = audioData;
+  const { audioName, duration, startPoint, bgColor, id } = audioData;
+  const { audioPills, setAudioPills } = useAudio()
+
+  const [isDragging, setIsDragging] = useState(false);
+  const [leftMargin, setLeftMargin] = useState(calculateOffset(startPoint, totalDuration));
+  const [startX, setStartX] = useState(0);
+  
+  const [selectedPill, setSelectedPill] = useState('');
+
+  const handleMouseDown = (e, id) => {
+    const parentWidth = e.currentTarget.parentElement.offsetWidth;
+    setIsDragging(true);
+    setStartX(
+      e.clientX - (leftMargin * parentWidth) / 100
+    );
+    setSelectedPill(id);
+  };
+
+  const handleMouseMove = (e) => {
+    if (isDragging) {
+      const newLeftMargin = e.clientX - startX;
+      const parentWidth = e.currentTarget.parentElement.offsetWidth; // Get the parent's width
+      const componentWidth = e.currentTarget.offsetWidth; // Get the component's width
+
+      // Calculate the minimum and maximum allowed left margins
+      const minLeftMargin = 0;
+      const maxLeftMargin = parentWidth - componentWidth;
+
+      // Clamp the newLeftMargin to stay within the bounds
+      const clampedLeftMargin = Math.max(
+        minLeftMargin,
+        Math.min(newLeftMargin, maxLeftMargin)
+      );
+
+      const percentMargin = (clampedLeftMargin / parentWidth) * 100;
+
+      setLeftMargin(percentMargin);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsDragging(false);
+    setAudioPills(prevPills => {
+      return prevPills.map(pill => {
+        if(pill.id === selectedPill){
+          pill.startPoint = (totalDuration * leftMargin) / 100;
+        }
+        return pill;
+      })
+    })
+  };
+
+  useEffect(() => {
+    setLeftMargin(calculateOffset(startPoint, totalDuration));
+  }, [audioPills])
+
   return (
     <div className={styles.timelineRow}>
       <div
-        style={{ 
+        style={{
           width: `${calculateWidth(duration, totalDuration)}%`,
-          marginLeft: `${calculateOffset(startPoint, totalDuration)}%`,
+          marginLeft: `${leftMargin}%`,
           backgroundColor: bgColor,
         }}
-        className={styles.rowPill}
+        className={`${styles.rowPill} ${isDragging ? styles.dragging : ""}`}
+        onMouseDown={(e) => handleMouseDown(e, id)}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseLeave={handleMouseUp}
       >
         <p>{audioName}</p>
       </div>
@@ -25,18 +84,21 @@ const TimelineRow = ({ audioData, totalDuration }) => {
 const Timeline = () => {
   const { audioPills } = useAudio();
 
-  const totalDuration = useMemo(
-    () => audioPills.reduce((acc, pill) => (acc = acc + pill.duration), 0),
-    [audioPills]
+  const totalDuration = audioPills.reduce(
+    (acc, pill) => (acc = acc + pill.duration),
+    0
   );
 
   return (
-    <div className={styles.timelineContainer}>
-      {audioPills.map((pill) => (
-        <TimelineRow audioData={pill} totalDuration={totalDuration} />
-      ))}
-      
-    </div>
+      <div className={styles.timelineContainer}>
+        {audioPills.map((pill) => (
+          <TimelineRow
+            key={pill.id}
+            audioData={pill}
+            totalDuration={totalDuration}
+          />
+        ))}
+      </div>
   );
 };
 
